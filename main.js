eef64f14781ca67a27d88cf53b35a3ab1be6bca8
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-01-30 15:35:13 +1000

    add js animations

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("test");
+const observer = new IntersectionObserver(
+  (animations) => {
+    animations.forEach((element) => {
+      if (element.isIntersecting) {
+        element.target.classList.add("animate");
+        element.target.classList.remove("undo-animate");
+      } else {
+        element.target.classList.remove("animate");
+        element.target.classList.add("undo-animate");
+      }
+    });
+  },
+  {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1],
+  }
+);
+
+const tags = document.querySelectorAll(".section-1-gifts-image, .tier-1, .tier-2, .tier-3");
+
+tags.forEach((tag) => {
+  observer.observe(tag);
+});
