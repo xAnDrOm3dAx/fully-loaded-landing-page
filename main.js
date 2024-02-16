50dd2cecf35c32b74a4418e33b520f74bbc7b038
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-02-16 15:29:44 +1000

    change var to const

--- main.js
+++ main.js
@@ -276,13 +276,13 @@ function clearPhoneError() {
 //
 //
 //Dropdown 3
-var accItem = document.getElementsByClassName("accordionItem");
-var accHD = document.getElementsByClassName("accordionItemHeading");
+const accItem = document.getElementsByClassName("accordionItem");
+const accHD = document.getElementsByClassName("accordionItemHeading");
 for (i = 0; i < accHD.length; i++) {
   accHD[i].addEventListener("click", toggleItem, false);
 }
 function toggleItem() {
-  var itemClass = this.parentNode.className;
+  const itemClass = this.parentNode.className;
   for (i = 0; i < accItem.length; i++) {
     accItem[i].className = "accordionItem close";
   }
